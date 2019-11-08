b73cf5e990d56d0ff441dc4504d15735f6474808
Wanglanhua2 <2968035392.com>
2019-11-08 16:12:02 +0800

    hotfix

--- src/com/test/controller.java
+++ src/com/test/controller.java
@@ -3,5 +3,6 @@ package com.test;
 public class controller {
 public static void main(String[] args) {
 	System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
+	System.out.println("hot fix");
 }
 }
